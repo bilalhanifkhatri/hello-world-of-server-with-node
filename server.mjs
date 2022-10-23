990fb4df9adf5b20a2c4f74d7d89253b3af2ea89
Ibtihaj Ali <ibtihaj.ali51@gmail.com>
2022-10-23 21:42:55 +0500

    initial commit

---
+++ server.mjs
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/water", (req, res) => {
+  console.log(`${req.ip} is asking for water.`);
+  res.send(`<h1>Take this water</h1>`);
+});
+
+app.get("/food", (req, res) => {
+  console.log(`${req.ip} is asking for food.`);
+  res.send(`<h1>Take this food</h1>`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
